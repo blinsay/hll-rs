580c6cdbd732e69d55c4d2da69ba39ff23d4ed88
Ben Linsay <blinsay@gmail.com>
2018-03-26 23:40:06 -0400

    make the example a little cleaner

--- examples/experiment.rs
+++ examples/experiment.rs
@@ -3,61 +3,47 @@ extern crate rand;
 
 use hll::HLL;
 use rand::Rng;
+
+use std::cmp::Eq;
 use std::collections::HashSet;
+use std::hash::Hash;
 
 fn main() {
     let samples = 10 * 1_000_000;
+    let measure_every = 100_000;
 
     let mut rng = rand::thread_rng();
     let mut explicit = HashSet::new();
     let mut approx = HLL::new(16, 5);
 
-    println!("n,explicit,hll,error");
+    // print a header before starting
+    println!("iterations,exact_cardinality,hll_cardinality,measurement_error");
 
-    for i in 0..samples {
+    for i in 1..(samples + 1) {
         let e: u64 = rng.gen();
         explicit.insert(e);
         approx.add_raw(e);
 
-        if i % (5 * 10_000) == 0 {
-            let stat = SetStat::from(i, &explicit, &approx);
-            println!(
-                "{},{},{},{:.06}",
-                stat.n,
-                stat.explicit,
-                stat.approximate,
-                stat.pct_error(),
-            )
+        if i % measure_every == 0 {
+            let Observation(n, e, a, err) = observe(i, &explicit, &approx);
+            println!("{},{},{},{:0.6}", n, e, a, err);
         }
     }
-
-    let stat = SetStat::from(samples, &explicit, &approx);
-    println!(
-        "{},{},{},{:.06}",
-        stat.n,
-        stat.explicit,
-        stat.approximate,
-        stat.pct_error(),
-    );
 }
 
-struct SetStat {
-    n: usize,
-    explicit: usize,
-    approximate: f64,
+struct Observation(usize, usize, f64, f64);
+
+fn observe<T: Hash + Eq>(n: usize, explicit: &HashSet<T>, hll: &HLL) -> Observation {
+    let exact = explicit.len();
+    let approximate = hll.cardinality_estimate();
+    Observation(
+        n,
+        exact,
+        approximate,
+        measurement_error(exact as f64, approximate),
+    )
 }
 
-impl SetStat {
-    fn from<T: std::hash::Hash + Eq>(n: usize, explicit: &HashSet<T>, hll: &HLL) -> Self {
-        Self {
-            n: n,
-            explicit: explicit.len(),
-            approximate: hll.cardinality_estimate(),
-        }
-    }
-
-    fn pct_error(&self) -> f64 {
-        let explicit = self.explicit as f64;
-        (explicit - self.approximate) / explicit
-    }
+fn measurement_error(actual: f64, estimate: f64) -> f64 {
+    (estimate - actual) / actual
 }
